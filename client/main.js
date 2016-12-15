cc6a0ee9f567694682bdc99c09cd381f15017f93
zzpengg <cfps91152@gmail.com>
2016-12-15 22:03:31 +0800

    the basic

--- client/main.js
+++ client/main.js
@@ -1,22 +1,16 @@
-import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 
-import './main.html';
+class App extends Component {
+  render(){
+    return (
+      <div>Hello there!</div>
+    )
+  };
+};
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
-});
-
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
-});
-
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
+// After Meteor loads in the browser, render my app to the DOM
+Meteor.startup( () => {
+  // React render call
+  ReactDOM.render(<App />, document.querySelector('.container'));
 });
